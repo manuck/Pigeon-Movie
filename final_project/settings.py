41a90432a69721f77e2058238bf6106e0142474b
jeongSangyoung <mycampground@daum.net>
2019-05-16 07:07:50 +0000

    .ebextensions

--- final_project/settings.py
+++ final_project/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'accounts.MyUser'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# python manage.py collectstatic
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
